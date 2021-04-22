a6d0ea1df7fbbc8be3d776c65a86ea260546cd71
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-04-22 13:57:56 -0300

    chore: add axios and changed request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,34 @@
-//SPA
-//SSR
-//SSG
+import { GetStaticProps } from 'next'
+import api from '../services/api'
 
-export default function Home({ episodes }) {
-  console.log(episodes)
-  return <h1>Index</h1>
+type Episode = {
+  id: string
+  title: string
+  members: string
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+type HomeProps = {
+  episodes: Episode[]
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  })
+  const data = response.data
 
   return {
     props: {
